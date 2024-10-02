836d270e7d6c244cea85a7914775b8629b94e477
zksx <52799997+zksx@users.noreply.github.com>
2024-10-02 12:50:01 -0700

    Refactor book position and rotation to be in it's own function. Added helper function to visualize book direction.

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function init(){
 	scene.add(light);
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
-	camera.position.z = 5;
+	camera.position.z = 3;
 
 	resizeRendererToDisplaySize(renderer)
 
@@ -37,20 +37,14 @@ loader.load( 'book.glb', function  ( gltf ) {
 	gltf.scene.scale.set(2,2,2);
 	scene.add( gltf.scene );
 	book = gltf;
-	book.scene.position.x = -0.8
-	book.scene.position.y = 1.0
-	book.scene.position.z = -5.0
-	book.scene.rotation.x = 0;
-	book.scene.rotation.y = 3.0;
-	book.scene.rotation.z = 0.0;
-	book_rotate_position();
+	book_set_position();
+	book_set_rotate();
 	mixer = new THREE.AnimationMixer(gltf.scene)
 	const clips = gltf.animations;
 	const clip = THREE.AnimationClip.findByName(clips, 'Animation');
 	const action = mixer.clipAction(clip);
 	action.play();
 	renderer.setAnimationLoop(animate);
-
 
 }, undefined, function ( error ) {
 
@@ -59,26 +53,53 @@ loader.load( 'book.glb', function  ( gltf ) {
 }
 
 );
-
-
-}
-
-function book_rotate_position(){
-	book.scene.position.x = -0.8
-	book.scene.position.y = 1.0
-	book.scene.position.z = -5.0
-	book.scene.rotation.x = 0;
-	book.scene.rotation.y = 3.0;
-	book.scene.rotation.z = 0.0;
 }
 
 
 function animate() 
 {
-
 		mixer.update(clock.getDelta());
 		renderer.render(scene, camera);
+}
 
+
+function book_helper()
+{
+	const axesHelper = new THREE.AxesHelper( 5 ); scene.add( axesHelper );
+	const dir = new THREE.Vector3( rotation_x, rotation_y, rotation_z ); 
+	const origin = new THREE.Vector3( 0, 0, 0 ); 
+	const length = 1; const hex = 0xffff00; 
+	const arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex ); 
+
+	dir.normalize(); 
+	scene.add( arrowHelper );
+}
+
+
+function book_set_position()
+{
+	book.scene.position.x = -0.8
+	book.scene.position.y = 1.0
+	book.scene.position.z = -5.0
+}
+
+function book_set_rotate(){
+	let rotation_x = 2;
+	let rotation_y = 0;
+	let rotation_z = 0;
+
+	book.scene.rotation.x = rotation_x;
+	book.scene.rotation.y = rotation_y;
+	book.scene.rotation.z = rotation_z;
+}
+
+
+function slide_book()
+{
+	if(book.scene.position.x <= 0.5)
+	{
+		book.scene.position.x += 0.03;
+	}
 }
 
 
@@ -95,13 +116,6 @@ function throw_down() {
 	}
 }
 
-function slide_book()
-{
-	if(book.scene.position.x <= 0.5)
-	{
-		book.scene.position.x += 0.03;
-	}
-}
 
 function resizeRendererToDisplaySize(renderer) {
 	const canvas = renderer.domElement;
